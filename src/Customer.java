f7e5d7be6604e28638600b744555db6f573fde1e
oExcellent <waltm3@mail.broward.edu>
2017-04-28 18:20:26 -0400

    Connected GUI to class Started the form continuation

--- src/Customer.java
+++ src/Customer.java
@@ -3,28 +3,22 @@ public class Customer {
 	String name;
 	String address;
 	String phoneNumber;
+	String accountNumber;
 	
 		Customer(){
 			name = "Max";
 			address = "123 up wy";
 			phoneNumber = "9541112222";
+			accountNumber= "MW-2121";
 		}
-		Customer(String n, String a, String p){
+		Customer(String n, String a, String p, String aN){
 			this.name = n;
 			this.address = a;
 			this.phoneNumber = p;
+			this.accountNumber= aN;
 		}
 		
-		public String getName(){
-			return this.name;
-		}
-		public String getAddress(){
-			return this.address;
-		}
-		public String getPhoneNumber(){
-			return this.phoneNumber;
-		}
-		
+		//Setters
 		public void setName(String n){
 			this.name = n;
 		}
@@ -34,5 +28,24 @@ public class Customer {
 		public void setPhoneNumber(String p){
 			this.phoneNumber = p;
 		}
+		public void setAccountNumber(String a){
+			this.accountNumber = a;
+		}
+		
+		//Getters
+		public String getName(){
+			return this.name;
+		}
+		public String getAddress(){
+			return this.address;
+		}
+		public String getPhoneNumber(){
+			return this.phoneNumber;
+		}
+		public String getAccountNumber(){
+			return this.accountNumber;
+		}
+		
+
 
 }
--- src/CustomerDriver.java
+++ src/CustomerDriver.java
@@ -4,10 +4,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 public class CustomerDriver extends JFrame {
+public static Integer trackAdd =0;
 
 	private JPanel contentPane;
+	private JTextField nameTF;
+	private JTextField addressTF;
+	private JTextField phoneTF;
 
 	/**
 	 * Launch the application.
@@ -33,8 +43,83 @@ public class CustomerDriver extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblFullName = new JLabel("Full Name:");
+		lblFullName.setBounds(6, 21, 91, 16);
+		contentPane.add(lblFullName);
+		
+		JLabel lblAddress = new JLabel("Address: ");
+		lblAddress.setBounds(6, 49, 61, 16);
+		contentPane.add(lblAddress);
+		
+		JLabel lblPhoneNumber = new JLabel("Phone Number:");
+		lblPhoneNumber.setBounds(6, 77, 107, 16);
+		contentPane.add(lblPhoneNumber);
+		
+		nameTF = new JTextField();
+		nameTF.setToolTipText("");
+		nameTF.setBounds(145, 16, 130, 26);
+		contentPane.add(nameTF);
+		nameTF.setColumns(10);
+		
+		addressTF = new JTextField();
+		addressTF.setBounds(145, 44, 130, 26);
+		contentPane.add(addressTF);
+		addressTF.setColumns(10);
+		
+		phoneTF = new JTextField();
+		phoneTF.setBounds(145, 72, 130, 26);
+		contentPane.add(phoneTF);
+		phoneTF.setColumns(10);
+		//create variable to track button clicks
+		trackAdd = 1;
+		//create button event
+		JButton ContinueBTN = new JButton("Continue");
+		ContinueBTN.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//set variables based on text input
+				String name = nameTF.getText();
+				String address = addressTF.getText();
+				String number = phoneTF.getText();
+				
+				//create customer instance
+				Customer cust1 = new Customer();
+				cust1.setName(name);
+				cust1.setAddress(address);
+				cust1.setPhoneNumber(number);
+				
+				//remove the text fields and the label
+				if (trackAdd > 0){
+				contentPane.remove(nameTF);
+				contentPane.remove(addressTF);
+				contentPane.remove(phoneTF);
+				
+				contentPane.remove(lblFullName);
+				contentPane.remove(lblAddress);
+				contentPane.remove(lblPhoneNumber);
+				}
+				
+				
+				JLabel displayName = new JLabel(cust1.getName());
+				displayName.setBounds(180, 21, 120, 36);
+				contentPane.add(displayName);
+				
+				JLabel displayAddress = new JLabel(cust1.getAddress());
+				displayAddress.setBounds(180, 49, 120, 36);
+				contentPane.add(displayAddress);
+				
+				JLabel DisplayNumber = new JLabel(cust1.getPhoneNumber());
+				DisplayNumber.setBounds(180, 77, 120, 36);
+				contentPane.add(DisplayNumber);
+				
+				//repaint!!!!!
+				contentPane.revalidate();
+				contentPane.repaint();
+			}
+		});
+		ContinueBTN.setBounds(158, 186, 117, 29);
+		contentPane.add(ContinueBTN);
 	}
-
 }
